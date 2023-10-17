99fc4377f876260ec1ebc0a3cdbae671a79da1ea
alessioaldi <aldinucci.alessio@itismeucci.com>
2023-10-17 13:39:14 +0200

    gioco finito

--- client/client/src/main/java/com/example/App.java
+++ client/client/src/main/java/com/example/App.java
@@ -5,26 +5,32 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         try {
             Socket s = new Socket("localhost", 3000);
-        
+            Scanner scan = new Scanner(System.in);
+
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        
-            out.writeBytes(" sium\n");
-            String r = in.readLine();
-        
-            System.out.println("Il server ha risporso:" + r);
+
+            int r = 0;
+
+            do {
+                out.writeBytes(scan.nextLine()+ "\n");
+
+                r = Integer.parseInt(in.readLine());
+
+                System.out.println("Il server ha risporso: " + r);
+            } while (r != 3);
+
+            System.out.println("numero indovinato in " + Integer.parseInt(in.readLine()) + " tentativi");
             s.close();
-        
-            } catch (Exception e) {
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            }
+        }
     }
 }
-
--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -12,11 +12,9 @@ import java.util.Random;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
 
         Random rand = new Random();
         try {
@@ -24,29 +22,30 @@ public class App
             ServerSocket server = new ServerSocket(3000);
             Socket s = server.accept();
             int n = rand.nextInt(101);
-            int tent=0;
+            int tent = 0;
+            int nRic = 0;
+            System.out.println(n);
 
             System.out.println("in client si è ocnnesso");
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-            do{
-                
-                int nRic = in.read();
-                tent++;
-                if(nRic>n){
-                    out.writeByte(1);
-                } else if(nRic<n){
-                    out.writeByte(2);
-                } else{
-                    out.writeByte(3);
-                }
-                
-                
-                
-            }while();
+            do {
 
-            out.writeByte(tent);
+                nRic = Integer.parseInt(in.readLine());
+                tent++;
+                if (nRic > n) {
+                    out.writeBytes("1\n");
+                } else if (nRic < n) {
+                    out.writeBytes("2\n");
+                } else {
+                    out.writeBytes("3\n");
+                    System.out.println("il client ha indovinato\n");
+                }
+
+            } while (nRic != n);
+
+            out.writeBytes(Integer.toString(tent) + "\n");
             server.close();
             s.close();
         } catch (Exception e) {
