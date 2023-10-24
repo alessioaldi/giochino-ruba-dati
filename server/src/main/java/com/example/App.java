a2cf4621cd50a306fc4b270cba2d7ff78999dd67
lai2-24 <lai2-24@itismeucci.com>
2023-10-24 13:02:18 +0200

    finito(forse)

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -18,36 +18,18 @@ public class App {
 
         Random rand = new Random();
         try {
-            System.out.println("Server avviato");
             ServerSocket server = new ServerSocket(3000);
-            Socket s = server.accept();
-            int n = rand.nextInt(101);
-            int tent = 0;
-            int nRic = 0;
-            System.out.println(n);
-
-            System.out.println("in client si è ocnnesso");
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-            do {
-
-                nRic = Integer.parseInt(in.readLine());
-                tent++;
-                if (nRic > n) {
-                    out.writeBytes("1\n");
-                } else if (nRic < n) {
-                    out.writeBytes("2\n");
-                } else {
-                    out.writeBytes("3\n");
-                    System.out.println("il client ha indovinato\n");
-                }
-
-            } while (nRic != n);
-
-            out.writeBytes(Integer.toString(tent) + "\n");
-            server.close();
-            s.close();
+            
+            while(true){
+                Socket s = server.accept();
+                System.out.println("Server avviato");
+                
+                Frate f = new Frate(s, server);
+                f.start();
+            }
+            
+            
+            
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(1);
